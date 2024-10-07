9ca8c51bdb099d2620a24ec632f2a2957c1e673e
박진영 <tear4321@cau.ac.kr>
2024-10-07 18:32:44 +0900

    Background Part Linker 제작 ing..

---
+++ Assets/Script/JYP/BackgroundPartLinker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundPartLinker : MonoBehaviour
+{
+    public List<BackgroundPart> BackgroundParts = new List<BackgroundPart>();
+
+    public void Create(string backgroundName, EBackgroundParkType type)
+    {
+        if(BackgroundParts.Exists(x => x.Name == backgroundName)) return;
+        
+        BackgroundParts.Add(new BackgroundPart
+        {
+            Name = backgroundName,
+            Type = type,
+            LinkedParts = new List<BackgroundPart>()
+        });
+    }
+    
+    public void UpdatePart(string originalName, string backgroundName, EBackgroundParkType type)
+    {
+        
+        var backgroundPart = BackgroundParts.Find(x => x.Name == originalName);
+        if (backgroundPart == null) return;
+        
+        backgroundPart.Name = backgroundName;
+        backgroundPart.Type = type;
+    }
+    
+    public void Delete(string backgroundName)
+    {
+        var backgroundPart = BackgroundParts.Find(x => x.Name == backgroundName);
+        
+        foreach (var part in backgroundPart.LinkedParts)
+        {
+            part.LinkedParts.Remove(backgroundPart);
+        }
+        
+        BackgroundParts.Remove(BackgroundParts.Find(x => x.Name == backgroundName));
+    }
+    
+    public void Link(BackgroundPart current, BackgroundPart next)
+    {
+        if (current.LinkedParts.Contains(next)) return;
+        current.LinkedParts.Add(next);
+        next.LinkedParts.Add(current);
+    }
+    
+    public void Unlink(BackgroundPart current, BackgroundPart next)
+    {
+        if (!current.LinkedParts.Contains(next)) return;
+        current.LinkedParts.Remove(next);
+        next.LinkedParts.Remove(current);
+    }
+}
