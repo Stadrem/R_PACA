9204fbc00fe78751a62b94aabf480d8f484b4e6a
박진영 <tear4321@cau.ac.kr>
2024-10-12 12:06:10 +0900

    데이터 입력단 준비

---
+++ Assets/Script/JYP/UI/CreateUniverseController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CreateUniverseController : MonoBehaviour
+{
+    public VisualTreeAsset objectiveItemTemplate;
+
+    private TextField titleInput;
+    private TextField genreInput;
+    private TextField contentInput;
+    private Label charactersLabel;
+    private Label backgroundsLabel;
+    private TextField tagsInput;
+    private ListView objectivesListView;
+    private ObjectiveListViewController objectivesListController;
+    
+    private List<string> objectives = new List<string>();
+    private List<string> characters = new List<string>();
+    private List<string> backgrounds = new List<string>();
+
+
+    private void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        titleInput = root.Q<TextField>("input_title");
+        genreInput = root.Q<TextField>("input_genre");
+        contentInput = root.Q<TextField>("input_content");
+        charactersLabel = root.Q<Label>("lbl_characters");
+        backgroundsLabel = root.Q<Label>("lbl_backgrounds");
+        tagsInput = root.Q<TextField>("input_tags");
+        objectivesListController = new ObjectiveListViewController();
+        objectivesListController.Initialize(root, objectiveItemTemplate);
+    }
+}
---
+++ Assets/Script/JYP/UI/ObjectiveListEntryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+public class ObjectiveListEntryController
+{
+    private VisualElement root;
+    private TextField objectiveInput;
+    private VisualElement addImage;
+
+    public void SetVisualElement(VisualElement visualElement)
+    {
+        objectiveInput = visualElement.Q<TextField>("input_objective");
+        addImage = visualElement.Q<VisualElement>("image_add");
+        root = visualElement;
+    }
+
+    public void BindData(ObjectiveListViewController.IObjectiveListEntry objective, Action addItem)
+    {
+        switch (objective)
+        {
+            case ObjectiveListViewController.ObjectiveListEntry entry:
+                root.RegisterCallback<ClickEvent>(e => { } );
+                objectiveInput.SetValueWithoutNotify(entry.objective);
+                objectiveInput.style.display = DisplayStyle.Flex;
+                addImage.style.display = DisplayStyle.None;
+                break;
+            case ObjectiveListViewController.AddObjectiveListEntry:
+                root.RegisterCallback<ClickEvent>(e => addItem());
+                objectiveInput.SetValueWithoutNotify("");
+                objectiveInput.style.display = DisplayStyle.None;
+                addImage.style.display = DisplayStyle.Flex;
+                break;
+        }
+    }
+}
