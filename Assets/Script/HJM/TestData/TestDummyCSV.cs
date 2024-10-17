52d46f51912e627c0bbe274a804013ce53ddeb97
goranirago <zimi749@naver.com>
2024-10-17 18:39:00 +0900

    feat: 세계관 더미데이터 입력 기능
    - 더미데이터 파싱

---
+++ Assets/Script/HJM/TestData/TestDummyCSV.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DummyCSV : MonoBehaviour
+{
+    static DummyCSV instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        Parse("uniDummyData");
+    }
+    public void Parse(string fileName) // 혹은 path
+    {
+        // flieName에 해당되는 file을 읽어오자
+        string path = Application.dataPath + "/Script/HJM/TestData/" + fileName + ".csv";
+        print(path);
+
+        string strData = File.ReadAllText(path);
+        print(strData);
+
+        // 엔터를 기준으로 한 줄씩 자르자
+
+        // "\n" 을 기준으로 자르자.
+        string [] lines = strData.Split("\n");
+        // "\r" 을 기준으로 자르자.
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string [] temp = lines[i].Split("\r");
+            lines[i] = temp[0];
+            print(lines[i]);
+        }
+    }
+}
