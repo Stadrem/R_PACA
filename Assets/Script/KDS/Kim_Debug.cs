94f9e51a2eb75e9cdce7c97a9f9b60ae24ff0854
Stadrem <stadrem@naver.com>
2024-10-25 13:14:28 +0900

    모험담 샘플 제작

---
+++ Assets/Script/KDS/Kim_Debug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kim_Debug : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            int result = DiceRollManager.Get().DiceRoll(3, 4);
+            Debug.Log("반환된 주사위 결과: " + result);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            YarnUiManager.Get().EnableCanvas();
+        }
+    }
+}
---
+++ Assets/Script/KDS/YarnUiManager.cs
@@ -0,0 +1,65 @@
+﻿using Photon.Pun.UtilityScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YarnUiManager : MonoBehaviour
+{
+    //싱글톤
+    public static YarnUiManager instance;
+
+    public static YarnUiManager Get()
+    {
+        if (instance == null)
+        {
+            // 프리팹 로드
+            GameObject prefabToInstantiate = Resources.Load<GameObject>("scripts/Ui/Canvas_Yarn");
+            if (prefabToInstantiate != null)
+            {
+                // 프리팹 생성
+                GameObject newInstance = Instantiate(prefabToInstantiate);
+                instance = newInstance.GetComponent<YarnUiManager>();
+
+                if (instance == null)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return instance;
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public GameObject page2;
+    public GameObject page3;
+
+    public void EnableCanvas()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void OnClickPage2()
+    {
+        page2.SetActive(true);
+    }
+
+    public void OnClickPage3()
+    {
+        page3.SetActive(true);
+    }
+}
