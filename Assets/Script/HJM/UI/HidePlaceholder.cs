c3d091e53060f01457f94cb24f102f9790eb6568
goranirago <zimi749@naver.com>
2024-11-01 21:27:29 +0900

    feat: 스탯설정UI 마우스오버, 스탯 저장
    - 스탯설정 창에 마우스를 올리면 설명창 UI가 뜸
    - 입력한 생명력, 힘, 손재주 스탯을 UserInfo에 저장

---
+++ Assets/Script/HJM/UI/HidePlaceholder.cs
@@ -0,0 +1,71 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputFieldPlaceholder : MonoBehaviour, IPointerClickHandler
+{
+    private TMP_InputField inputField;   // TMP_InputField 컴포넌트
+    private TMP_Text placeholderText; // Placeholder 텍스트 객체
+
+    void Start()
+    {
+        // TMP_InputField 컴포넌트를 가져옴
+        inputField = GetComponent<TMP_InputField>();
+
+        // Placeholder 텍스트 컴포넌트를 가져옴
+        if (inputField.placeholder != null)
+        {
+            placeholderText = inputField.placeholder.GetComponent<TMP_Text>();
+        }
+
+        // 시작 시 Placeholder 상태 설정
+        ShowPlaceholder();
+    }
+
+    // 마우스 클릭 시 호출되는 함수
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        HidePlaceholder();
+    }
+
+    // Placeholder를 숨기는 함수
+    void HidePlaceholder()
+    {
+        if (placeholderText != null)
+        {
+            placeholderText.enabled = false;
+        }
+    }
+
+    // Placeholder를 다시 보이게 하는 함수
+    public void ShowPlaceholder()
+    {
+        // 입력된 텍스트가 없는 경우에만 Placeholder를 보이게 함
+        if (string.IsNullOrEmpty(inputField.text))
+        {
+            if (placeholderText != null)
+            {
+                placeholderText.enabled = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        // 입력란의 내용이 변경될 때마다 Placeholder 상태를 업데이트
+        if (inputField != null)
+        {
+            if (string.IsNullOrEmpty(inputField.text)) // 수정된 부분
+            {
+                ShowPlaceholder();
+            }
+            else
+            {
+                if (placeholderText != null)
+                {
+                    placeholderText.enabled = false; // 텍스트가 있으면 Placeholder 숨김
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/HJM/UI/MouseOverUIStats.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseOverUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject DescriptionUI;
+    public Vector3 offset = new Vector3(-180f, -15f, 0f); // 설명창이랑 커서가 떨어진 거리
+
+    private bool isPointerOver = false; // 마우스가 스탯 아이콘 위에 있는지
+
+    private void Start()
+    {
+        DescriptionUI.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isPointerOver = true;
+        DescriptionUI.SetActive(true);
+        UpdateTooltipPosition();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerOver = false;
+        DescriptionUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isPointerOver)
+        {
+            UpdateTooltipPosition();
+        }
+    }
+
+    private void UpdateTooltipPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition + offset;
+        DescriptionUI.transform.position = mousePosition;
+    }
+}
