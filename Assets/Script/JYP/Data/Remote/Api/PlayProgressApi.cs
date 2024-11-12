31f936e50b9345ea742a991315993db45e7d06d4
박진영 <tear4321@cau.ac.kr>
2024-11-12 10:23:37 +0900

    api/draft/PlayProgrees StartNpcTalk

---
+++ Assets/Script/JYP/Data/Remote/Api/PlayProgressApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using Data.Remote.Dtos.Request;
+
+namespace Data.Remote.Api
+{
+    [System.Serializable]
+    public class PlayProgressApi
+    {
+        private static readonly string BaseUrl = $"{HttpManager.ServerURL}/progress";
+
+        public static IEnumerator StartNpcTalk(int roomNumber, string backgroundName, string npcName, Action<ApiResult> onComplete)
+        {
+            var reqDto = new StartTalkReqDto()
+            {
+                roomNumber = roomNumber,
+                backgroundName = backgroundName,
+                npcName = npcName,
+            };
+
+            var request = new HttpInfoWithType<string, StartTalkReqDto>()
+            {
+                url = $"{BaseUrl}/start",
+                body = reqDto,
+                onComplete = (result) => { onComplete(ApiResult.Success()); },
+                onError = (error) => onComplete(ApiResult.Fail(error)),
+                
+            };
+
+            yield return HttpManager.GetInstance().Post(request);
+        }
+    }
+}
---
+++ Assets/Script/JYP/Data/Remote/Dtos/Request/StartTalkReqDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Remote.Dtos.Request
+{
+    [System.Serializable]
+    public class StartTalkReqDto
+    {
+        public int roomNumber;
+        public string backgroundName;
+        public string npcName;
+    }
+}
