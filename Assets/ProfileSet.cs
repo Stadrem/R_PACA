9a1aa6a45fb606e7cf8843c0c428827d4468fcdd
goranirago <zimi749@naver.com>
2024-11-18 17:19:48 +0900

    fix: 플레이어 애니메이션 동기화, Hp바 txt 추가
    - 플레이어 애니메이션 포톤 동기화
    - Hp바 값을 txt로 표시

--- Assets/ProfileSet.cs
+++ Assets/ProfileSet.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 
 public class ProfileSet : MonoBehaviour
 {
-
     public TMP_Text nickName;
     public Slider hpBar;
     public List<Image> selects; // [0]: 공격, [1]: 방어
+    public TMP_Text hp;
 
-    private float currentHealthPercentage;  // 체력 비율을 저장하는 변수
+    int maxHealth;
 
     public void NicknameSet(string name)
     {
@@ -21,18 +21,26 @@ public class ProfileSet : MonoBehaviour
     // 체력 초기화
     public void HpBarInit(int health)
     {
+        maxHealth = health;  // 최대 체력
         hpBar.maxValue = health;
         hpBar.value = hpBar.maxValue;
+        UpdateHpText();// 초기 체력
     }
 
     // 데미지
     public void DamagedPlayer(int damage)
     {
         float damagePercentage = hpBar.value - damage;
-        hpBar.value = damagePercentage;
-      
+        hpBar.value = Mathf.Max(damagePercentage, 0);
+        UpdateHpText();
     }
 
+    // HP 텍스트 갱신
+    private void UpdateHpText()
+    {
+        int currentHp = Mathf.RoundToInt(hpBar.value);
+        hp.text = $"{currentHp} / {maxHealth}";
+    }
 
     public void SetSelectImage(int select) // select[0]: 선택안함, [1]: 공격, [2]: 방어
     {
@@ -51,5 +59,3 @@ public class ProfileSet : MonoBehaviour
         }
     }
 }
-
-
