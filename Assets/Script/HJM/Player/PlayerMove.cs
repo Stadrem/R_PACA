269a1c8974f5b45c59bc60d948ad6cf6f777b256
goranirago <zimi749@naver.com>
2024-11-19 22:54:27 +0900

    add: 이동클릭 시 이펙트 변경
    - 이동클릭할 때 나오는 이펙트 변경

--- Assets/Script/HJM/Player/PlayerMove.cs
+++ Assets/Script/HJM/Player/PlayerMove.cs
@@ -26,7 +26,7 @@ public class PlayerMove : MonoBehaviourPun
 
     // 클릭 이동 가능 여부
     public bool clickMovementEnabled = true;
-
+    GameObject currentClickEffect;
     // 서버에서 넘어오는 위치값
     Vector3 receivePos;
     // 서버에서 넘어오는 회전값
@@ -68,7 +68,6 @@ public class PlayerMove : MonoBehaviourPun
             }
         }
     }
-
     void ClickToMove()
     {
         // 클릭 이동이 활성화되어 있을 때만 이동 아니면 나감
@@ -77,21 +76,38 @@ public class PlayerMove : MonoBehaviourPun
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
         {
-            // 목적지를 hit 포인트로 설정
             agent.destination = hit.point;
 
+            if (currentClickEffect != null)
+            {
+                Destroy(currentClickEffect);
+            }
+
             // 클릭 이펙트 생성
-            GameObject clickEffect = Instantiate(Resources.Load("ClickEffect") as GameObject, hit.point + new Vector3(0, 0.1f, 0), Quaternion.identity);
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            Vector3 adjustedRotation = rotation.eulerAngles;
+            adjustedRotation.x += 90;
+            currentClickEffect = Instantiate(Resources.Load("ClickEffect01") as GameObject, hit.point + new Vector3(0, 0.1f, 0), Quaternion.Euler(adjustedRotation));
 
-            // 일정 시간이 지나면 이펙트를 삭제
-            StartCoroutine(DestroyAfterTime(clickEffect, 2.0f)); // 2초 후 삭제됨
+            StartCoroutine(DestroyEffectOnArrival(currentClickEffect));
         }
-
     }
-    IEnumerator DestroyAfterTime(GameObject go, float delay)
+
+    IEnumerator DestroyEffectOnArrival(GameObject effect)
     {
-        yield return new WaitForSeconds(delay);  // delay 초 대기
-        Destroy(go); // go를 삭제함
+        while (agent.pathPending || agent.remainingDistance > 0.1f)
+        {
+            yield return null;
+        }
+        if (effect != null)
+        {
+            Destroy(effect);
+
+            if (currentClickEffect == effect)
+            {
+                currentClickEffect = null;
+            }
+        }
     }
     void OnEnable()
     {
@@ -133,8 +149,8 @@ public class PlayerMove : MonoBehaviourPun
         {
             if (agent.velocity == Vector3.zero)
             {
-                animator.SetBool("isIdle", true);      
-                animator.SetBool("isWalk", false);      
+                animator.SetBool("isIdle", true);
+                animator.SetBool("isWalk", false);
             }
             else
             {
