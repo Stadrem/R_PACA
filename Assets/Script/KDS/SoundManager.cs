0ec896093f04a5a259e20210d390e14bf85ede73
Stadrem <stadrem@naver.com>
2024-11-19 10:46:22 +0900

    BGM 시스템 개선

--- Assets/Script/KDS/SoundManager.cs
+++ Assets/Script/KDS/SoundManager.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     //싱글톤
     public static SoundManager instance;
+
+    Scene currentScene;
+
+    int currentBGM = -1;
+
+    int previousBGM = -1;
 
     public static SoundManager Get()
     {
@@ -46,6 +53,29 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        // 현재 씬의 이름을 가져옴
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // 씬이 로드될 때마다 호출되는 함수
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 현재 씬의 이름을 가져옴
+        currentScene = SceneManager.GetActiveScene();
+
+        ChangedBGM();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDestroy()
+    {
+        // 오브젝트가 파괴될 때 이벤트 등록 해제 (중복 방지)
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     void Update()
@@ -76,8 +106,81 @@ public class SoundManager : MonoBehaviour
         sfxAudioSource.PlayOneShot(sfxClips[num], sfxVolume);
     }
 
-    public void PlayBGM(int num)
+    public void PlayBGM(int i)
     {
-        bgmAudioSource.PlayOneShot(bgmClips[num], bgmVolume);
+        if (currentBGM != i)
+        {
+            previousBGM = currentBGM;
+
+            bgmAudioSource.Stop();
+
+            bgmAudioSource.clip = bgmClips[i];
+
+            bgmAudioSource.volume = bgmVolume;
+
+            bgmAudioSource.loop = true;
+
+            bgmAudioSource.Play();
+
+            currentBGM = i;
+        }
+    }
+
+    public void PlayBGM(int i, float volume)
+    {
+        if (currentBGM != i)
+        {
+            previousBGM = currentBGM;
+
+            bgmAudioSource.Stop();
+
+            bgmAudioSource.clip = bgmClips[i];
+
+            bgmAudioSource.volume = bgmVolume * volume;
+
+            bgmAudioSource.loop = true;
+
+            bgmAudioSource.Play();
+
+            currentBGM = i;
+        }
+    }
+
+    // 볼륨 변경 메서드 추가
+    public void SetBGMVolume(float volume)
+    {
+        bgmVolume = volume;
+
+        // 현재 재생 중인 BGM의 볼륨 업데이트
+        if (bgmAudioSource.isPlaying)
+        {
+            bgmAudioSource.volume = bgmVolume;
+        }
+    }
+
+    void ChangedBGM()
+    {
+        if(currentScene.name == "MainScene")
+        {
+            PlayBGM(0, 1);
+        }
+        else if(currentScene.name == "LobbyScene")
+        {
+            PlayBGM(1, 0.5f);
+        }
+        else if(currentScene.name == "Town")
+        {
+            PlayBGM(2, 0.5f);
+        }
+    }
+
+    public void BattleBGMCall()
+    {
+        PlayBGM(3, 0.5f);
+    }
+
+    public void BattleBGMEnd()
+    {
+        PlayBGM(previousBGM, 0.5f);
     }
 }
