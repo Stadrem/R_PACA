39b70338f50a0f5dc7206b82ff23f5611da0c76b
goranirago <zimi749@naver.com>
2024-11-22 11:04:25 +0900

    feat: 던전 씬에 전투 기능 추가
    - 던전 씬에 전투 기능 추가해둠

---
+++ Assets/Script/HJM/UI/ScrollViewController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewController : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public float step = 0.2f;
+
+    public void MoveRight()
+    {
+        float newPosition = Mathf.Clamp(scrollRect.horizontalNormalizedPosition + step, 0, 1);
+        scrollRect.horizontalNormalizedPosition = newPosition;
+    }
+
+    public void MoveLeft()
+    {
+        float newPosition = Mathf.Clamp(scrollRect.horizontalNormalizedPosition - step, 0, 1);
+        scrollRect.horizontalNormalizedPosition = newPosition;
+    }
+}
