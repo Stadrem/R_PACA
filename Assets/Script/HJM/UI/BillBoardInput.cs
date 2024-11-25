2aa7999ddbe8fbfa42f1784f493855ac89fe8cfc
goranirago <zimi749@naver.com>
2024-11-25 18:00:12 +0900

    fix: BillBoard 수정
    - BillBoard 수정 안쓰는거 정리

--- Assets/Script/HJM/UI/BillBoardInput.cs
+++ Assets/Script/HJM/UI/BillBoardInput.cs
@@ -1,17 +1,17 @@
-﻿using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using Photon.Pun;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class BillBoardInput : MonoBehaviour
-{
-    public void CamInput()
-    {
-        Billboard[] bill = GameObject.FindObjectsOfType<Billboard>();
+//public class BillBoardInput : MonoBehaviour
+//{
+//    public void CamInput()
+//    {
+//        Billboard[] bill = GameObject.FindObjectsOfType<Billboard>();
 
-        for (int i = 0; i < bill.Length; i++)
-        {
-            bill[i].cam = transform.gameObject;
-        }
-    }
-}
+//        for (int i = 0; i < bill.Length; i++)
+//        {
+//            bill[i].cam = transform.gameObject;
+//        }
+//    }
+//}
