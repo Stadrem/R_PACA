43a772274df29190b8136841440395c8ead1ded9
goranirago <zimi749@naver.com>
2024-11-26 13:31:59 +0900

    fix: Town씬에 우클릭안내멘트 빌보드 수정
    - 우클릭 안내멘트 덜덜 거리던거 수정

---
+++ Assets/Script/HJM/UI/BillboardStart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class BillboardStart : MonoBehaviourPun
+{
+    private float timer = 0f;
+    private float duration = 0.5f;
+
+    private void LateUpdate()
+    {
+        if (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            // 현재 활성화된 카메라 가져오기
+            Camera currentCamera = Camera.main;
+
+            if (currentCamera != null)
+            {
+                // 항상 카메라를 바라보도록 회전
+                transform.LookAt(transform.position + currentCamera.transform.rotation * Vector3.forward,
+                                 currentCamera.transform.rotation * Vector3.up);
+            }
+        }
+    }
+}
